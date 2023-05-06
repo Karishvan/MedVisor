da58080af139d48ac087edab0257c1cf4db6ebdd
Ammar Hakim <ammarhakim57@gmail.com>
2023-05-06 15:45:51 -0400

    Update script.js
    Question updating + result storing

--- templates/script.js
+++ templates/script.js
@@ -1,5 +1,6 @@
 const questions = document.querySelectorAll(".question");
 const nextButton = document.querySelectorAll(".next");
+let results = "";
 
 let currentQuestion = 0;
 
@@ -12,6 +13,9 @@ function hideQuestion() {
 }
 
 function nextQuestion() {
+  const response = getResponse();
+  results += ". " + response;
+  console.log("CURRENT RESULTS ARE " + results)
   hideQuestion();
   currentQuestion++;
   if (currentQuestion < questions.length) {
@@ -21,11 +25,23 @@ function nextQuestion() {
   }
 }
 
+function getResponse() {
+  const question = questions[currentQuestion];
+  const responseElement = question.querySelector(".response");
+  if (responseElement.querySelector("input[type=radio]")) {
+    const checkedInput = responseElement.querySelector("input[type=radio]:checked");
+    return checkedInput.value;
+  } else if (responseElement.querySelector("input[type=range]")) {
+    const rangeInput = responseElement.querySelector("input[type=range]");
+    return rangeInput.value;
+  } else if (responseElement.querySelector("input[type=text]")) {
+    const textInput = responseElement.querySelector("input[type=text]");
+    return textInput.value;
+  }
+}
 
 showQuestion();
 
 nextButton.forEach(button => {
   button.addEventListener("click", nextQuestion);
 });
-
-
